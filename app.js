75f93a04a38d113def889b3be59726757a6edb7a
Abhipray Dumka <dumkaabhipray@gmail.com>
2024-01-16 19:53:08 +0530

    deploy this

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-const helmet = require('helmet');
-const morgan = require('morgan');
-const compression = require('compression');
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// const helmet = require("helmet");
+// const morgan = require("morgan");
+// const compression = require("compression");
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-const route = require('./router');
-const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+app.use(express.static(path.join(__dirname, "public")));
+const route = require("./router");
+// const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+//   flags: "a",
+// });
 app.use(route);
-app.use(helmet());
-app.use(compression());
-app.use(morgan('combined'))
+// app.use(helmet());
+// app.use(compression());
+// app.use(morgan('combined'))
 var port_number = process.env.PORT || 3000;
 app.listen(port_number);
